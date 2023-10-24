723f379c860bd97017806b993cce20756669515e
Harsh Kumar <harsh341kumar@gmail.com>
2023-10-24 18:09:42 +0530

    formik added in login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,70 @@
+import { Formik } from 'formik';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
 
 const Login = () => {
-  const handleSubmit = e => e.preventDefault();
-
   return (
     <>
       <div className="form_wrapper">
         <h2 className="form_heading">Log in</h2>
-        <form noValidate onSubmit={handleSubmit}>
-          <div className="form_control">
-            <input name="email" type="email" placeholder="Email" />
-          </div>
-          <div className="form_control">
-            <input name="password" type="password" placeholder="Password" />
-          </div>
-          <div className="form_control">
-            <button type="submit">Log in</button>
-          </div>
-        </form>
+        <Formik
+          initialValues={{
+            email: '',
+            password: ''
+          }}
+          validationSchema={Yup.object().shape({
+            email: Yup.string().email('Invalid email').required('Required'),
+            password: Yup.string()
+              .min(8, 'Password must be 8 characters or long')
+              .required('Required')
+          })}
+          onSubmit={async (values, { setSubmitting }) => {
+            try {
+              console.log(values);
+              setTimeout(() => {
+                setSubmitting(false);
+              }, 10002);
+            } catch (error) {
+              console.log(error);
+              setSubmitting(false);
+            }
+          }}
+        >
+          {({ values, errors, handleChange, handleSubmit, isSubmitting }) => (
+            <form noValidate onSubmit={handleSubmit}>
+              <div className="form_control">
+                <input
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  onChange={handleChange}
+                  value={values.email}
+                />
+                {errors.email && (
+                  <span className="input_err">{errors.email}</span>
+                )}
+              </div>
+              <div className="form_control">
+                <input
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  onChange={handleChange}
+                  value={values.password}
+                />
+                {errors.password && (
+                  <span className="input_err">{errors.password}</span>
+                )}
+              </div>
+              <div className="form_control">
+                <button type="submit" disabled={isSubmitting}>
+                  Log in
+                </button>
+              </div>
+            </form>
+          )}
+        </Formik>
         <div className="form_link">
           <Link to="/signup">Don't have an account? Sign up.</Link>
         </div>
